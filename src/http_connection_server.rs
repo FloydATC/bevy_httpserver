b2dfbf43d7a2fa727876556aac0ac4a84be3259b
FloydATC <floyd@atc.no>
2023-04-26 13:07:19 +0200

    Added tests, removed console spam

--- src/http_connection_server.rs
+++ src/http_connection_server.rs
@@ -17,8 +17,6 @@ See also: HttpConnectionTask
 use std::sync::{Arc, Mutex};
 use std::thread;
 
-use bevy::prelude::*;
-
 use vebb::*;
 
 use super::HttpClientConnection;
@@ -45,10 +43,8 @@ impl HttpConnectionServer {
     }
 
     pub fn run(&mut self) -> Result<(), String> {
-        info!("running");
         loop {
             // Read request from client and put it in self.request
-            info!("read request");
             match vebb::read_request(self.connection.reader()) {
                 Err(status) => {
                     return Err(format!("{}: {}", self.connection.peer(), status));
@@ -62,12 +58,11 @@ impl HttpConnectionServer {
             }
 
             // Wait for response to become ready
-            info!("wait for response");
             while !self.has_response() { thread::yield_now(); }
 
             // Take response from self.response, send it to the client
-            info!("send response");
             let response = self.take_response();
+
             let keep_alive = vebb::keep_alive_granted(&response);
             if let Err(os_error) = vebb::send_response(response, self.connection.writer()) {
                 if os_error.kind() == std::io::ErrorKind::ConnectionAborted { break; } // Connection closed by peer
@@ -78,7 +73,6 @@ impl HttpConnectionServer {
         }
 
         // Connection closed by peer
-        info!("stopping");
         match self.connection.close() {
             Err(os_error) => {
                 if os_error.kind() == std::io::ErrorKind::ConnectionAborted { return Ok(()) }
@@ -106,3 +100,193 @@ impl HttpConnectionServer {
 
 }
 
+
+#[cfg(test)]
+#[allow(dead_code)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new() {
+        let (_server, client) = HttpClientConnection::loopback().unwrap();
+        let _connserv = HttpConnectionServer::new(
+            client,
+            Arc::new(Mutex::new(None)),
+            Arc::new(Mutex::new(None)),
+        );
+        assert!(true);
+    }
+
+    #[test]
+    fn has_response_none() {
+        let (_server, client) = HttpClientConnection::loopback().unwrap();
+        let connserv = HttpConnectionServer::new(
+            client,
+            Arc::new(Mutex::new(None)),
+            Arc::new(Mutex::new(None)),
+        );
+        assert_eq!(connserv.has_response(), false);
+    }
+
+    #[test]
+    fn has_response_some() {
+        let (_server, client) = HttpClientConnection::loopback().unwrap();
+        let response = Response::builder().status(StatusCode::OK).body(Bytes::from_static(b"")).unwrap();
+        let connserv = HttpConnectionServer::new(
+            client,
+            Arc::new(Mutex::new(None)),
+            Arc::new(Mutex::new(Some(response))),
+        );
+        assert_eq!(connserv.has_response(), true);
+    }
+
+    #[test]
+    fn set_request() {
+        let (_server, client) = HttpClientConnection::loopback().unwrap();
+        let arc_req = Arc::new(Mutex::new(None));
+        let mut connserv = HttpConnectionServer::new(
+            client,
+            arc_req.clone(),
+            Arc::new(Mutex::new(None)),
+        );
+        let request = Request::builder()
+            .method(Method::GET)
+            .uri("/".parse::<Uri>().unwrap())
+            .body(Bytes::from_static(b""))
+            .unwrap();
+
+        connserv.set_request(Some(request));
+        assert_eq!(arc_req.lock().unwrap().is_some(), true);
+    }
+
+    #[test]
+    #[should_panic]
+    fn take_response_none() {
+        let (_server, client) = HttpClientConnection::loopback().unwrap();
+        let mut connserv = HttpConnectionServer::new(
+            client,
+            Arc::new(Mutex::new(None)),
+            Arc::new(Mutex::new(None)),
+        );
+        let _response: Response<Bytes> = connserv.take_response();
+    }
+
+    #[test]
+    fn take_response_some() {
+        let (_server, client) = HttpClientConnection::loopback().unwrap();
+        let response = Response::builder().status(StatusCode::OK).body(Bytes::from_static(b"")).unwrap();
+        let mut connserv = HttpConnectionServer::new(
+            client,
+            Arc::new(Mutex::new(None)),
+            Arc::new(Mutex::new(Some(response))),
+        );
+        assert_eq!(connserv.has_response(), true);
+        let _response: Response<Bytes> = connserv.take_response();
+        assert_eq!(connserv.has_response(), false);
+    }
+
+    #[test]
+    fn run_client_close() {
+        let (mut server, client) = HttpClientConnection::loopback().unwrap();
+        server.close().expect("close failed");
+        let mut connserv = HttpConnectionServer::new(
+            client,
+            Arc::new(Mutex::new(None)),
+            Arc::new(Mutex::new(None)),
+        );
+        let handle = thread::spawn(move || connserv.run());
+        let _ = handle.join().expect("run() crashed");
+        assert!(true)
+    }
+
+    #[test]
+    fn run_keepalive_not_requested() {
+        let (mut server, client) = HttpClientConnection::loopback().unwrap();
+        let request = Request::builder()
+            .version(Version::HTTP_11)
+            .method(Method::GET)
+            .uri("/foo".parse::<Uri>().unwrap())
+            .header("Host", "localhost")
+            .body(Bytes::from_static(b""))
+            .unwrap();
+        vebb::send_request(request, server.writer()).expect("send_request failed");
+        let arc_req = Arc::new(Mutex::new(None));
+        let arc_res = Arc::new(Mutex::new(None));
+        let mut connserv = HttpConnectionServer::new(
+            client,
+            arc_req.clone(),
+            arc_res.clone(),
+        );
+        thread::spawn(move || connserv.run());
+        while arc_req.lock().unwrap().is_none() { thread::yield_now(); }
+        let _request: Request<Bytes> = arc_req.lock().unwrap().take().unwrap();
+        let response = Response::builder()
+            .status(StatusCode::OK)
+            .body(Bytes::from_static(b""))
+            .unwrap();
+        *arc_res.lock().unwrap() = Some(response);
+        assert!(true)
+    }
+
+    #[test]
+    fn run_keepalive_requested_but_denied() {
+        let (mut server, client) = HttpClientConnection::loopback().unwrap();
+        let request = Request::builder()
+            .version(Version::HTTP_11)
+            .method(Method::GET)
+            .uri("/foo".parse::<Uri>().unwrap())
+            .header("Host", "localhost")
+            .header("Connection", "keep-alive")
+            .body(Bytes::from_static(b""))
+            .unwrap();
+        vebb::send_request(request, server.writer()).expect("send_request failed");
+        let arc_req = Arc::new(Mutex::new(None));
+        let arc_res = Arc::new(Mutex::new(None));
+        let mut connserv = HttpConnectionServer::new(
+            client,
+            arc_req.clone(),
+            arc_res.clone(),
+        );
+        thread::spawn(move || connserv.run());
+        while arc_req.lock().unwrap().is_none() { thread::yield_now(); }
+        let _request: Request<Bytes> = arc_req.lock().unwrap().take().unwrap();
+        let response = Response::builder()
+            .status(StatusCode::OK)
+            .header("Connection", "close")
+            .body(Bytes::from_static(b""))
+            .unwrap();
+        *arc_res.lock().unwrap() = Some(response);
+        assert!(true)
+    }
+
+    #[test]
+    fn run_keepalive_requested_but_ignored() {
+        let (mut server, client) = HttpClientConnection::loopback().unwrap();
+        let request = Request::builder()
+            .version(Version::HTTP_11)
+            .method(Method::GET)
+            .uri("/foo".parse::<Uri>().unwrap())
+            .header("Host", "localhost")
+            .header("Connection", "keep-alive")
+            .body(Bytes::from_static(b""))
+            .unwrap();
+        vebb::send_request(request, server.writer()).expect("send_request failed");
+        let arc_req = Arc::new(Mutex::new(None));
+        let arc_res = Arc::new(Mutex::new(None));
+        let mut connserv = HttpConnectionServer::new(
+            client,
+            arc_req.clone(),
+            arc_res.clone(),
+        );
+        thread::spawn(move || connserv.run());
+        while arc_req.lock().unwrap().is_none() { thread::yield_now(); }
+        let _request: Request<Bytes> = arc_req.lock().unwrap().take().unwrap();
+        let response = Response::builder()
+            .status(StatusCode::OK)
+            .body(Bytes::from_static(b""))
+            .unwrap();
+        *arc_res.lock().unwrap() = Some(response);
+        assert!(true)
+    }
+
+}
