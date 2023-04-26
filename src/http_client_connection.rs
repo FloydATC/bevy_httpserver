43d71edb9133209c843b8312d77eac0dd2c1022b
FloydATC <floyd@atc.no>
2023-04-26 09:27:50 +0200

    Changed peer() to return a copy of SocketAddr rather than a ref. Added loopback() and this() for testing. Wrote tests.

--- src/http_client_connection.rs
+++ src/http_client_connection.rs
@@ -23,8 +23,29 @@ impl HttpClientConnection {
         }
     }
 
-    pub fn peer(&self) -> &std::net::SocketAddr {
-        return &self.peer;
+
+    // Convenience function for testing
+    pub fn loopback() -> Result<(Self, Self), std::io::Error> {
+        let listener = std::net::TcpListener::bind("127.0.0.1:0")?;
+
+        // From viewpoint of the client
+        let server_addr = listener.local_addr()?;
+        let server = std::net::TcpStream::connect(server_addr)?;
+        // From viewpoint of the server
+        let (client, client_addr) = listener.accept()?;
+
+        let conn1 = HttpClientConnection::new(server, server_addr);
+        let conn2 = HttpClientConnection::new(client, client_addr);
+        return Ok((conn1, conn2));
+    }
+
+    pub fn this(&self) -> std::net::SocketAddr {
+        return self.stream.local_addr().unwrap();
+        //return self.stream.;
+    }
+
+    pub fn peer(&self) -> std::net::SocketAddr {
+        return self.peer;
     }
 
     pub fn reader(&mut self) -> &mut std::io::BufReader<std::net::TcpStream> {
@@ -43,3 +64,61 @@ impl HttpClientConnection {
 
 }
 
+
+#[cfg(test)]
+#[allow(dead_code)]
+mod tests {
+    use std::io::Read;
+
+    use super::*;
+
+    #[test]
+    fn loopback() {
+        let (_server, _client) = HttpClientConnection::loopback().unwrap();
+        assert!(true);
+    }
+
+    #[test]
+    fn peer_not_null() {
+        let (server, _client) = HttpClientConnection::loopback().unwrap();
+        let null: std::net::SocketAddr = "0.0.0.0:0".parse().unwrap();
+        assert_ne!(server.peer(), null);
+    }
+
+    #[test]
+    fn this_not_null() {
+        let (server, _client) = HttpClientConnection::loopback().unwrap();
+        let null: std::net::SocketAddr = "0.0.0.0:0".parse().unwrap();
+        assert_ne!(server.this(), null);
+    }
+
+    #[test]
+    fn peer_ne_this() {
+        let (server, _client) = HttpClientConnection::loopback().unwrap();
+        assert_ne!(server.peer(), server.this());
+    }
+
+    #[test]
+    fn server_peer_eq_client_this() {
+        let (server, client) = HttpClientConnection::loopback().unwrap();
+        assert_eq!(server.peer(), client.this());
+    }
+
+    #[test]
+    fn client_peer_eq_server_this() {
+        let (server, client) = HttpClientConnection::loopback().unwrap();
+        assert_eq!(client.peer(), server.this());
+    }
+
+    #[test]
+    fn read_write() {
+        const READER: &[u8] = b"hello world";
+        let mut writer: [u8; READER.len()] = [0; READER.len()];
+        let (mut server, mut client) = HttpClientConnection::loopback().unwrap();
+        client.writer().write(&READER).expect("write failed");
+        client.close().expect("close failed"); // Must close, or reader() will block waiting for more data
+        server.reader().read_exact(&mut writer).expect("read failed");
+        assert_eq!(&READER[..], &writer[..]);
+    }
+
+}
